f0a59e1c3e833e7b3137b0521b5690c9c7fe11e8
Vuwan <sitaulabns@gmail.com>
2019-07-12 10:50:38 -0500

    a

--- nasaimagesearch/settings.py
+++ nasaimagesearch/settings.py
@@ -102,5 +102,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+if DEBUG:
+   STATICFILES_DIRS = [
+   os.path.join(BASE_DIR, 'static'),
+   ]
+else:
+   STATIC_ROOT = os.path.join(BASE_DIR,'static')
+
+import django_heroku
+django_heroku.settings(locals())
